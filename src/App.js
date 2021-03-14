869ef6f0575c1ea0ab87e49e55f211c73232daaf
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-14 10:10:18 +0300

    Add a path for quiz-property page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/pages/Landing/Landing";
+import QuizPropertyPage from './components/pages/QuizProperty/QuizProperty';
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" exact component={LandingPage} />
+        <Route path="/quiz-property"  component={QuizPropertyPage} />
       </Switch>
     </div>
   );
