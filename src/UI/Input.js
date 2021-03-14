ab897bccf8f440b5793ef95c5f8e68eee9f16165
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-14 10:11:55 +0300

    Create Input as a seperate component

---
+++ src/UI/Input.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  height: 32px;
+  width: 252px;
+  background-color: #1d0e2b;
+  border: none;
+  border-radius: 5px;
+  color: #f3e6ff;
+  text-align: center;
+  &:active {
+    border: #8030cd;
+  }
+  &:hover {
+    border: #8030cd;
+  }
+`;
