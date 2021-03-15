2d10a89b46744e7b2c96d66d6042f18dfebaecf1
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-15 14:59:58 +0300

    Create the questions component

---
+++ src/components/pages/Questions/Questions.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import MobileContainer from "../../../UI/Mobile-Container";
+import MainLogo from "../../../UI/MainLogo";
+import { Button } from "../../../UI/Button";
+import { Input } from "../../../UI/Input";
+import { NavLink } from "react-router-dom";
+import {
+  LuckIcon,
+  Container,
+  HeaderContainer,
+  CategoryName,
+  TheQuestion,
+  OptionsContainer,
+  FooterContainer,
+} from "./Questions.styles";
+
+const Questions = () => {
+  return (
+    <MobileContainer>
+      <HeaderContainer>
+        <CategoryName>Biology Quiz</CategoryName>
+        <Container>
+          <p>Question 1/2</p>
+          <LuckIcon />
+        </Container>
+      </HeaderContainer>
+      <TheQuestion>The energy released by 1 gram of glucose is ?</TheQuestion>
+      <OptionsContainer>
+        <Button secondary>100</Button>
+        <Button secondary>200</Button>
+        <Button secondary>300</Button>
+        <Button secondary>400</Button>
+      </OptionsContainer>
+      <FooterContainer>
+        <NavLink to="/quiz-property">Quit quiz :(</NavLink>
+        <Button primary>NEXT</Button>
+      </FooterContainer>
+    </MobileContainer>
+  );
+};
+
+export default Questions;
