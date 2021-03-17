acc8a3642335a3a35b001c2c5d5e03ba10022d41
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-17 11:47:56 +0300

    Rename the questions to quiz

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/pages/Landing/Landing";
-import QuizPropertyPage from './components/pages/QuizProperty/QuizProperty';
-import Questions from './components/pages/Questions/Questions';
+import QuizPropertyPage from "./components/pages/QuizProperty/QuizProperty";
+import Quiz from "./components/pages/Quiz/Quiz";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" exact component={LandingPage} />
-        <Route path="/quiz-property"  component={QuizPropertyPage} />
-        <Route path="/questions"  component={Questions} />
+        <Route path="/quiz-property" component={QuizPropertyPage} />
+        <Route path="/quiz" component={Quiz} />
       </Switch>
     </div>
   );
