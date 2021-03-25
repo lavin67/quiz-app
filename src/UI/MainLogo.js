ec44f70f52c512f74214344d203c01805a579644
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-25 10:41:12 +0300

    Add styles for the laptopM size

--- src/UI/MainLogo.js
+++ src/UI/MainLogo.js
@@ -1,5 +1,6 @@
 import { ReactComponent as Logo } from "../Icons/Logo.svg";
 import styled from "styled-components";
+import { sizes, devices } from "../Devices/Devices";
 
 const LogoContainer = styled.div`
   display: flex;
@@ -11,12 +12,26 @@ const LogoContainer = styled.div`
     font-size: 32px;
     color: ${(props) => props.theme.colors.textColor};
   }
+  @media ${devices.laptopM} {
+    max-width: ${sizes.laptopM};
+    margin-top: 0.6rem;
+    p {
+      font-size: 38px;
+    }
+  }
 `;
 
 const LogoSVG = styled(Logo)`
   width: 36px;
   height: 50px;
   margin-left: 8px;
+  @media ${devices.laptopM} {
+    max-width: ${sizes.laptopM};
+    width: 40px;
+    height: 72px;
+    margin-left: 8px;
+    
+  }
 `;
 
 const MainLogo = () => {
