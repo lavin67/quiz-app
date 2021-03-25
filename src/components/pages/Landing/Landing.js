406975ad26e228d1a8a46191b0d842c4274b833e
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-03-25 15:10:15 +0300

    Change Button to StyledButton and add some containers

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -1,28 +1,40 @@
 import React from "react";
 import MobileContainer from "../../../UI/Mobile-Container";
 import MainLogo from "../../../UI/MainLogo";
-import { Description, LearningIcon, LearningIconContainer } from "./Landing.styles";
-import { Button } from "../../../UI/Button";
+import {
+  Description,
+  LearningIcon,
+  LearningIconContainer,
+  MainContainerForLaptop,
+  MainContainer,
+  StyledButton,
+  PrimaryContainer,
+} from "./Landing.styles";
+//import { Button } from "../../../UI/Button";
 import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <MobileContainer>
-      <MainLogo />
-      <Description>
-        El-Quizo is an application that makes learning fun!
-        <br />
-        Answer all the questions correctly and win your favorite drink!
-      </Description>
+      <MainContainerForLaptop>
+        <MainContainer>
+          <MainLogo />
+          <Description>
+            El-Quizo is an application that makes learning fun!
+            <br />
+            Answer all the questions correctly and win your favorite drink!
+          </Description>
 
-      <NavLink to="/quiz-property">
-        <Button style={{ marginTop: "2rem" }} primary>
-          START THE QUIZ
-        </Button>
-      </NavLink>
-      <LearningIconContainer>
-        <LearningIcon />
-      </LearningIconContainer>
+          <NavLink to="/quiz-property">
+            <StyledButton primary>START THE QUIZ</StyledButton>
+          </NavLink>
+        </MainContainer>
+        <PrimaryContainer>
+          <LearningIconContainer>
+            <LearningIcon />
+          </LearningIconContainer>
+        </PrimaryContainer>
+      </MainContainerForLaptop>
     </MobileContainer>
   );
 };
