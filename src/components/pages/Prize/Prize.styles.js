c3ecdb9ed057bef25dc490989be6e11a9c943ca3
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-04-06 10:42:30 +0300

    Add styles

--- src/components/pages/Prize/Prize.styles.js
+++ src/components/pages/Prize/Prize.styles.js
@@ -10,14 +10,53 @@ import { ReactComponent as Rum } from "../../../Icons/rum.svg";
 import { ReactComponent as Tequila } from "../../../Icons/tequila.svg";
 import { ReactComponent as Vodka } from "../../../Icons/vodka.svg";
 import { ReactComponent as Cocktail } from "../../../Icons/cocktail.svg";
+import { sizes, devices } from "../../../Devices/Devices";
 
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-top: 3.5rem;
+  @media ${devices.laptopM} {
+    max-width: ${sizes.laptopM};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
-export const Description = styled.p``;
+export const Description = styled.p`
+  text-transform: capitalize;
+  color: ${(props) => props.theme.colors.textColor};
+  font-size: 28px;
+  @media ${devices.laptopM} {
+    font-size: 40px;
+  }
+`;
 
-export const PrizeContainer = styled.div``;
+export const PrizeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  svg {
+    height: 213px;
+    width: 9rem;
+    margin-bottom: 6rem;
+    margin-top: 3rem;
+  }
+  @media ${devices.laptopM} {
+    max-width: ${sizes.laptopM};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    svg{
+      width: 12rem;
+      height: 12rem;
+    }
+  }
+ 
+`;
