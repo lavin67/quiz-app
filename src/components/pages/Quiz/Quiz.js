338e4afce7f2ffd46c629de90dfc9ba093d32299
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-04-06 11:58:10 +0300

    Remove the testing button

--- src/components/pages/Quiz/Quiz.js
+++ src/components/pages/Quiz/Quiz.js
@@ -95,16 +95,7 @@ const Questions = () => {
   }, []);
 
 
-  const onQuestionAmountChange = () => {
-//     api
-//       .get(`https://opentdb.com/api_count.php?category=${selectedCategory}`)
-//       .then((e) => {
-//         console.log(e.data.category_question_count);
-//         if(amount > e.data.category_question_count){
-// console.log("u did wrong, too many question")
-//         }
-//       });
-  };
+
 
   return (
     <>
@@ -145,7 +136,7 @@ const Questions = () => {
                         </Button>
                       );
                     })}
-                    <Button onClick={() => onQuestionAmountChange()}>hi there</Button>
+                    
                   </OptionsContainer>
                   <FooterContainer>
                     {console.log(selectedAnswer)}
