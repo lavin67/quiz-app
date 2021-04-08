f7efdee0d7ec918a3c905e658f34bed0056dcfe1
lavin67 <69416919+lavin67@users.noreply.github.com>
2021-04-08 10:37:34 +0300

    Remove unnecessary imports

--- src/components/pages/Prize/Prize.styles.js
+++ src/components/pages/Prize/Prize.styles.js
@@ -1,15 +1,5 @@
 import styled from "styled-components";
-import { ReactComponent as Luck } from "../../../Icons/Luck.svg";
-import { Button } from "../../../UI/Button";
-import { NavLink } from "react-router-dom";
-import { ReactComponent as Whisky } from "../../../Icons/Whisky.svg";
-import { ReactComponent as Wine } from "../../../Icons/wine.svg";
-import { ReactComponent as Beer } from "../../../Icons/beer.svg";
-import { ReactComponent as Martini } from "../../../Icons/martini.svg";
-import { ReactComponent as Rum } from "../../../Icons/rum.svg";
-import { ReactComponent as Tequila } from "../../../Icons/tequila.svg";
-import { ReactComponent as Vodka } from "../../../Icons/vodka.svg";
-import { ReactComponent as Cocktail } from "../../../Icons/cocktail.svg";
+
 import { sizes, devices } from "../../../Devices/Devices";
 
 export const MainContainer = styled.div`
